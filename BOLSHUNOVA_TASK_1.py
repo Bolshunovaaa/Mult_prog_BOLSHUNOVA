1ee359e3787ac7273a131d8712a3dbaf085e316b
Semen1455 <semen.yakymenko@infiz.khpi.edu.ua>
2023-05-29 21:11:18 +0300

    Task_1

---
+++ BOLSHUNOVA_TASK_1.py
@@ -0,0 +1,8 @@
+# Multiparadigmatic  programming languages:Task_1
+# Bolshunova Katerina:IKM-221a
+
+print('Multiparadigmatic  programming languages:Task_1\nBolshunova Keterina:IKM-221a')
+
+print(('Vladislav, ' * 44)[:-2])
+
+print((11.2 / (11.2 - 211.3)) + (1.2 / (15.2 + 11.3)))
